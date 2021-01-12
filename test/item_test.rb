aeed42064ab07029b05fcf8fee726a7c952f32c3
Joseph Budina <joebud3@gmail.com>
2021-01-12 13:08:07 -0700

    Initialize item with attributes

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,8 +4,8 @@ require './lib/item'
 
 class ItemTest < Minitest::Test
   def setup
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   def test_it_exists_with_attributes
