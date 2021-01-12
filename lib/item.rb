8eeef99ab959e28f763b41d41d84116193a1db60
Joseph Budina <joebud3@gmail.com>
2021-01-12 13:14:30 -0700

    Initialize foodtruck

--- lib/item.rb
+++ lib/item.rb
@@ -1,12 +1,9 @@
 class Item 
-  attr_reader :name
+  attr_reader :name,
+              :price
 
   def initialize(data)
     @name = data[:name]
-    @price = data[:price]
-  end
-
-  def price 
-    @price[1..-1].to_f
+    @price = data[:price][1..-1].to_f
   end
 end
