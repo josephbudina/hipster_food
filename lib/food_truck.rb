d96b318a491a00b878da5500353110b8ccde30b8
Joseph Budina <joebud3@gmail.com>
2021-01-12 13:17:51 -0700

    Add check stock method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,6 +4,10 @@ class FoodTruck
 
   def initialize(name)
     @name = name
-    @inventory = Hash.new
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
   end
 end
