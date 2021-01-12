722dc03e9d1391f076698c773b27c5a59096625e
Joseph Budina <joebud3@gmail.com>
2021-01-12 13:06:31 -0700

    Add item exists test

---
+++ test/item_test.rb
@@ -0,0 +1,17 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/item'
+
+class ItemTest < Minitest::Test
+  def setup
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  def test_it_exists_with_attributes
+    assert_instance_of Item, @item1
+
+    assert_equal "Apple Pie (Slice)", @item2.name
+    assert_equal 2.50, @item2.price
+  end
+end
